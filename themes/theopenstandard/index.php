6b5875363311a3129d9c21ad6641eb3d7b29f833
Joren Mathews <joren@isimple.net>
2014-09-25 13:49:08 -0700

    Fixed git ignore rule. Added a bunch of index.php files

---
+++ themes/theopenstandard/index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage HTML5-Reset-WordPress-Theme
+ * @since HTML5 Reset 2.0
+ */
+ get_header(); ?>
+
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+        <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+
+            <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+
+            <?php posted_on(); ?>
+
+            <div class="entry">
+                <?php the_content(); ?>
+            </div>
+
+            <footer class="postmetadata">
+                <?php the_tags(__('Tags: ','html5reset'), ', ', '<br />'); ?>
+                <?php _e('Posted in','html5reset'); ?> <?php the_category(', ') ?> | 
+                <?php comments_popup_link(__('No Comments &#187;','html5reset'), __('1 Comment &#187;','html5reset'), __('% Comments &#187;','html5reset')); ?>
+            </footer>
+
+        </article>
+
+    <?php endwhile; ?>
+
+    <?php post_navigation(); ?>
+
+    <?php else : ?>
+
+        <h2><?php _e('Nothing Found','html5reset'); ?></h2>
+
+    <?php endif; ?>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
