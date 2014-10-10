05372aa0410cdf3befb05361eb2a8be82c4920c8
Rex Vokey <rex@isimple.net>
2014-10-10 16:04:17 -0700

    Pointing to local all.js, took http: off of other assets

--- themes/theopenstandard/header.php
+++ themes/theopenstandard/header.php
@@ -20,8 +20,8 @@
     <!-- concatenate and minify for production -->
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/reset.css" />
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" />
-    <link href='http://fonts.googleapis.com/css?family=Karma:400,300,500' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Fira+Sans:300,400,400italic,700' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Karma:400,300,500' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Fira+Sans:300,400,400italic,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/foundation/css/foundation.min.css" />
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/_/css/zurb.css" />
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/_/css/blog.css" />
@@ -47,7 +47,7 @@
         <div class="inner-wrap">
             <div class="nav-icon-and-logo">
                 <div>
-                    <a class="left-off-canvas-toggle" href="#"><img src="http://5c4cf848f6454dc02ec8-c49fe7e7355d384845270f4a7a0a7aa1.r53.cf2.rackcdn.com/assets/images/0992d7f691b654e889e30d9bed9196d2a9ec5891/menu-icon.svg"></a>
+                    <a class="left-off-canvas-toggle" href="#"><img src="//5c4cf848f6454dc02ec8-c49fe7e7355d384845270f4a7a0a7aa1.r53.cf2.rackcdn.com/assets/images/0992d7f691b654e889e30d9bed9196d2a9ec5891/menu-icon.svg"></a>
                 </div>
                 <div class="logo-container">
                     <a title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home" href="<?php echo esc_url(home_url('/')); ?>"><img src="http://5c4cf848f6454dc02ec8-c49fe7e7355d384845270f4a7a0a7aa1.r53.cf2.rackcdn.com/assets/images/30d897fbdac61ee6a0ea567077e5d5eb323ec341/tos_logo.svg"></a>
